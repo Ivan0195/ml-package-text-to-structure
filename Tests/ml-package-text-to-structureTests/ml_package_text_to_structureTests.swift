743bf4f034cfb192870864fdc7ec3ea016197b7f
Ivan0195 <iihnatsenka95@gmail.com>
2024-04-23 14:15:14 +0300

    Initial Commit

---
+++ Tests/ml-package-text-to-structureTests/ml_package_text_to_structureTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import ml_package_text_to_structure
+
+final class ml_package_text_to_structureTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
